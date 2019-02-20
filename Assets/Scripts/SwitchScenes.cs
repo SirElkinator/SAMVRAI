fb88eabe64e49035ef5e47e74cf1d0fc470830aa
Chase Sparks <chaspa97@att.net>
2019-02-19 21:52:48 -0600

    SceneSwitch Button Active

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes: MonoBehaviour {
+    public void ChangeScene(string sceneName) { SceneManager.LoadScene("scene1"); }
+}
