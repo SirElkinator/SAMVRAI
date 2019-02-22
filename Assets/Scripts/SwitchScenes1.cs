ad243a99d8a19177fe5ea789366520817b2d326a
Chase Sparks <chaspa97@att.net>
2019-02-21 23:07:59 -0600

    Added credits scene

---
+++ Assets/Scripts/SwitchScenes1.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes1: MonoBehaviour {
+    public void ChangeScene(string sceneName) { SceneManager.LoadScene("Credits"); }
+}
