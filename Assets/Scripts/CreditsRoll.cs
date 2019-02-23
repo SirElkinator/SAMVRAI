20a416bc8e153e7e405d4b37a81f64a4a0a15cd6
Chase Sparks <chaspa97@att.net>
2019-02-22 21:19:38 -0600

    Credits roll now

---
+++ Assets/Scripts/CreditsRoll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsRoll : MonoBehaviour {
+    public GameObject creators;
+    public GameObject music;
+    int count = 0;
+    // Use this for initialization
+    void Start () {
+        creators.SetActive(true);
+        music.SetActive(false);
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        count = count + 1;
+        if (count > 200)
+        {
+            creators.SetActive(false);
+            music.SetActive(true);
+        }
+	}
+}
